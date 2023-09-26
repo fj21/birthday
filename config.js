3266250a168ed88d01b562cc4a9368b64f90f2c6
fj21 <91861292+fj21@users.noreply.github.com>
2023-09-26 20:06:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "安乐窝~": "baobao.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -42,8 +42,8 @@ var config = {
         play: "音乐",
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        cake_fadein: "蛋糕",
+        light_candle: "蜡烛",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
